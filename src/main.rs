c088249153b90a22172d1784104bf75954c48e22
Khush Mehta <contact@khushm.com>
2025-09-05 01:21:33 +0530

    feat: winit setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use winit::error::EventLoopError;
+use winit::event_loop::{ControlFlow, EventLoop};
+
+use crate::app::App;
+
+fn main() -> Result<(), EventLoopError> {
+    let event_loop = EventLoop::new()?;
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app)
 }
